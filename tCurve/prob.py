0aa79b08f603f704a3747bcad8d2b1babbc1fdf2
peixiongau <70298430+peixiongau@users.noreply.github.com>
2021-02-15 17:15:06 +0800

    Red Light |

--- tCurve/prob.py
+++ tCurve/prob.py
@@ -121,7 +121,7 @@ def _integrate(t, n, _f):
         oldResult = newResult
         step = (highB - lowB) / slice
         newResult = 0
-        for i in range(0, highB/slice - 2, step):
+        for i in range(0, slice-2):
             newResult += _f(lowB+step, n) + 4 * _f(lowB + step, n) + _f(lowB + 2*step, n)
         newResult *= (step/3)
         
